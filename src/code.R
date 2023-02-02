f4e36b4fe417ce71348cf2ab1f0de0344674d7ba
Valentin <valentin.richard39@sfr.fr>
2023-02-02 12:02:05 +0100

    premieres regressions

--- src/code.R
+++ src/code.R
@@ -17,9 +17,19 @@
 
 
 spotifyDF <- as.data.frame(read.csv("data/data.csv"))
-# print(summary(spotifyDF))
+print(summary(spotifyDF))
 
-modele1 <- lm(energy ~ danceability, data = spotifyDF)
+modele1 <- lm(energy ~ valence, data = spotifyDF)
 # plot(modele1)
-print(coef(modele1))
-print(summary(modele1))
+# print(coef(modele1))
+# print(summary(modele1))
+
+modele2 <- lm(liked ~ duration_ms + tempo + valence + energy + danceability + loudness + instrumentalness, data = spotifyDF)
+# plot(modele2)
+# print(coef(modele2))
+# print(summary(modele2))
+
+modele3 <- lm(danceability ~ instrumentalness + energy + valence + loudness + speechiness, data = spotifyDF)
+# plot(modele3)
+print(coef(modele3))
+print(summary(modele3))
